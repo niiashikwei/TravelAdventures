a3844b547deb541b2a1c284e1e4fd92eac1248b6
Nii <niiashikwei@hotmail.com>
2016-10-26 19:14:51 -0400

    Nii - inital commit

---
+++ app/src/main/java/ideaz/traveladventures/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package ideaz.traveladventures.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ideaz.traveladventures.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
