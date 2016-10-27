cc1dc825ecb1917de9311bd423702471d64f0405
Nii <niiashikwei@hotmail.com>
2016-10-26 20:03:20 -0400

    Nii - some config for facebook

---
+++ app/src/main/java/TravelAdventuresApplication.java
@@ -0,0 +1,15 @@
+import android.app.Application;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+public class TravelAdventuresApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
+    }
+}
--- app/src/main/java/ideaz/traveladventures/activities/MainActivity.java
+++ app/src/main/java/ideaz/traveladventures/activities/MainActivity.java
@@ -1,7 +1,7 @@
 package ideaz.traveladventures.activities;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import ideaz.traveladventures.R;
 
